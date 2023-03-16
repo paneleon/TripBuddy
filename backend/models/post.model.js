cf672942f3a594183fb1fcf263d0026471b21333
Eleonora <paneleon06@gmail.com>
2023-03-16 18:31:15 -0400

    Add Post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const Category = {
+    Restaurant,
+    Residence,
+    Attractions,
+    Educational,
+    Outdoors,
+    Cultural,
+    Religious,
+    Other
+}
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    rating: Number,
+    postedBy: String,
+    date: { type: Date, default: Date.now },
+    image: String,
+    Category: Category,
+    address: String,
+    country: String,
+    city: String,
+    comments: [{ 
+        body: String, 
+        date: Date, 
+        postedBy: String 
+    }], 
+    likes: [String], //list of user ids
+}, { timestamps: true}) // will give properties createdAt and updatedAt
+
+module.exports = mongoose.model('Post', postSchema);
