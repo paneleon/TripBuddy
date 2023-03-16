882e714e2a71c581d3026472e2f747d9378fd793
Eleonora <paneleon06@gmail.com>
2023-03-16 19:02:13 -0400

    Add API endpoint to create a new post and map the route

---
+++ backend/controllers/posts.controller.js
@@ -0,0 +1,28 @@
+const Post = require('../models/post.model')
+
+exports.createNewPost = async (req, res) => {
+
+    try {
+        if (!req.body.title){
+            return res.status(400).send({message: "Post title is required"})
+        }
+
+        const userId = "user id placeholder" // TODO: get user id from authentication middleware
+
+        const post = new Post({
+            title: req.body.title,
+            description: req.body.description,
+            rating: req.body.rating,
+            postedBy: userId,
+            image: req.body.image,
+            category: req.body.category,
+            address: req.body.address,
+            country: req.body.country,
+            city: req.body.city,
+        })
+        await post.save()
+        return res.status(200).json(post);
+    } catch (error) {
+        return res.status(500).send({message: `Server error: ${error.message}`})
+    }
+}
