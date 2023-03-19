4d7e299fbec8b7d23ccd94b83f50ef6d2ee33b14
Eleonora <paneleon06@gmail.com>
2023-03-19 00:00:37 -0400

    Hardcode credentials in authContext

--- frontend/src/context/authContext.js
+++ frontend/src/context/authContext.js
@@ -2,15 +2,23 @@
 import React, { useState } from 'react';
 const AuthContext = React.createContext();
 
-
+// USE THIS CREDENTIALS TO UPDATE HARDCODED TOKEN: 
+// username: chandler123
+// password: 123
 const AuthContextProvider = ({children}) => {
-    const [token, setToken] = useState("hello there")
+    const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MTY3YjVjZTVkZGQ0N2JlNzBiMjhmMiIsImlhdCI6MTY3OTE5Nzc3NSwiZXhwIjoxNjgxNzg5Nzc1fQ.Z5amVlx4ZXlWozbDT2igKqxLj8k6mStl1jCWlFSXsUw") // TODO: update after login and save to local storage
+    const [userId, setUserId] = useState("64167b5ce5ddd47be70b28f2") // TODO: update after login and save to local storage
     const [user, setUser] = useState({
-        firstName: "Ella",
-        lastName: "Panussyants"
+        // hardcoded user, fetch info from the db and save to local storage after
+        firstName: "Chandler",
+        lastName: "Bing",
+        email: "cbing@gmail.com",
+        username: "chandler123"
     })
 
     const value = { 
+        userId: userId,
+        setUserId: setUserId,
         token: token,
         setToken: setToken,
         user: user, 
