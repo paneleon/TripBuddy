1c0b3c76f8470fac76da90b901a8e7a0029331b0
JunhuaGuan <113260738+JunhuaGuan@users.noreply.github.com>
2023-03-20 15:17:08 -0400

    Update EditPostConfirmationPopup.js

---
+++ frontend/src/components/EditPostConfirmationPopup.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import styles from '../styles/Popup.module.css'
+import cn from 'classnames'
+import { editPost } from '../../../backend/controllers/posts.controller';
+
+const ConfirmationPopup = ({title, message, show, setShow, doAction}) => {
+
+           const title = "Publish Post Comfirmation Require"
+           const message = "Are you sure you want to publish this post ?"
+
+           const postEdit = () => {
+            editPost();
+            setShow(false)
+        }
+
+  return (
+    <>
+        {show &&
+        <div
+            className={cn('modal show', styles.popup)}
+            style={{ display: 'block', position: 'initial' }}
+            >
+            <Modal.Dialog>
+                <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                <p>{message}</p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShow(false)}>Back</Button>
+                <Button variant="success" onClick={() => doAction(postEdit)}>Confirm</Button>
+                </Modal.Footer>
+            </Modal.Dialog>
+            </div>
+        }
+    </>
+  )
+}
+
+export default ConfirmationPopup
