529dddd0c37973d9e808834a99d55354244e125e
JunhuaGuan <113260738+JunhuaGuan@users.noreply.github.com>
2023-03-20 15:52:21 -0400

    Add Post ConfirmationPopup

---
+++ frontend/src/components/ConfirmationPopup.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import styles from '../styles/Popup.module.css'
+import cn from 'classnames'
+import { createNewPost } from '../../../backend/controllers/posts.controller';
+
+const ConfirmationPopup = ({title, message, show, setShow, doAction}) => {
+
+           const title = "Publish Post Comfirmation Require"
+           const message = "Are you sure you want to publish this post ?"
+
+           const addpost = () =>
+           {
+              createNewPost()
+              setShow(false)
+           }
+
+  return (
+    <>
+        {show &&
+        <div
+            className={cn('modal show', styles.popup)}
+            style={{ display: 'block', position: 'initial' }}
+            >
+            <Modal.Dialog>
+                <Modal.Header closeButton>
+                <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                <p>{message}</p>
+                </Modal.Body>
+
+                <Modal.Footer>
+                <Button variant="secondary" onClick={() => setShow(false)}>Back</Button>
+                <Button variant="success" onClick={() => doAction(addpost)}>Confirm</Button>
+                </Modal.Footer>
+            </Modal.Dialog>
+            </div>
+        }
+    </>
+  )
+}
+
+export default ConfirmationPopup
