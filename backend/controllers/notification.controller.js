05f352dea5dfa36c099421100275344a49310193
JunhuaGuan <113260738+JunhuaGuan@users.noreply.github.com>
2023-04-04 19:05:52 -0400

    delete notification function

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -19,3 +19,17 @@ exports.createNewNotification = async (req, res) => {
           .send({ success: false, message: `Server error: ${error.message}` });
       }
 }
+
+exports.deleteNotification = async (req, res) => {
+  try {
+    const notificationId = req.params.id;
+    const notification = await Notification.findById(notificationId);
+    if (!notification) {
+      return res.status(404).send({ success: false, message: `Notification not found` })
+    }
+    await Notification.deleteOne({ _id: notificationId });
+    return res.status(200).json({ success: true, message: `Notification was successfully deleted` });
+  } catch (error) {
+    return res.status(500).send({ success: false, message: `Server error: ${error.message}` });
+  }
+}
