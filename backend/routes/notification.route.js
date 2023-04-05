9cbb75b7f9e3eb1ac1d3e6aa9f66e165c2a1fb8d
JunhuaGuan <113260738+JunhuaGuan@users.noreply.github.com>
2023-04-05 15:41:24 -0400

    Delete notification.route.js

--- backend/routes/notification.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const NotificationController = require('../controllers/notification.controller')
-const authUtils = require('../utils/auth.js')
-
-router.post('/addNotification', authUtils.isAuthenticated, NotificationController.createNewNotification)//API to add a new notification
-router.delete('/deleteById/:id', authUtils.isAuthenticated, NotificationController.deleteNotification)
-
-module.exports = router;
