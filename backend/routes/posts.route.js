769f453a34ff4be4b5e79034c7eb452639762f9d
Eleonora <paneleon06@gmail.com>
2023-04-09 13:55:17 -0400

    Update request to get statistics

--- backend/routes/posts.route.js
+++ backend/routes/posts.route.js
@@ -22,7 +22,7 @@ router.get('/likes/:id', PostsController.getlikes)
 router.post('/report', authUtils.isAuthenticated, PostsController.reportPost)
 router.get('/getReported', authUtils.isAuthenticated, PostsController.getReportedPosts)
 router.get('/suggestions', PostsController.getSuggestions)
-router.get('/getUserPostStats', authUtils.isAuthenticated, PostsController.getUserPostStats)
+router.get('/getUserPostStats/:id', authUtils.isAuthenticated, PostsController.getUserPostStats)
 router.put('/recordViewed/:id',PostsController.recordView)//API to record each view on post
 
 module.exports = router;
